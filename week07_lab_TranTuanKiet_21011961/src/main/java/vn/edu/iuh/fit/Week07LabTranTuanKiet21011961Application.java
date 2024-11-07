87b105e234ae53c76b4084763f264532396703f9
Kiệt Trần <trantuankiet5980@gmail.com>
2024-11-07 18:52:39 +0700

    Candidate paging

--- week07_lab_TranTuanKiet_21011961/src/main/java/vn/edu/iuh/fit/Week07LabTranTuanKiet21011961Application.java
+++ week07_lab_TranTuanKiet_21011961/src/main/java/vn/edu/iuh/fit/Week07LabTranTuanKiet21011961Application.java
@@ -20,31 +20,5 @@ public class Week07LabTranTuanKiet21011961Application {
     public static void main(String[] args) {
         SpringApplication.run(Week07LabTranTuanKiet21011961Application.class, args);
     }
-    @Autowired
-    private CandidateRepository candidateRepository;
-    @Autowired
-    private AddressRepository addressRepository;
-    @Bean
-    CommandLineRunner initData(){
-        return args -> {
-            Random rnd =new Random();
-            for (int i = 1; i < 1000; i++) {
-                Address add = new Address();
-                add.setNumber(rnd.nextInt(1,1000)+"");
-                add.setStreet("Quang Trung");
-                add.setCity("HCM");
-                add.setZipcode(rnd.nextInt(70000,80000)+"");
-                add.setCountry(CountryCode.getByCode(1));
-                addressRepository.save(add);
 
-                Candidate can=new Candidate("Name #"+i,
-                        LocalDate.of(1998,rnd.nextInt(1,13),rnd.nextInt(1,29)),
-                        add,
-                        "email_"+i+"@gmail.com",
-                        rnd.nextLong(1111111111L,9999999999L)+"");
-                candidateRepository.save(can);
-                System.out.println("Added: " +can);
-            }
-        };
-    }
 }
--- week07_lab_TranTuanKiet_21011961/src/main/java/vn/edu/iuh/fit/frontend/controllers/CandidateController.java
+++ week07_lab_TranTuanKiet_21011961/src/main/java/vn/edu/iuh/fit/frontend/controllers/CandidateController.java
@@ -1,11 +1,19 @@
 package vn.edu.iuh.fit.frontend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import vn.edu.iuh.fit.backend.entities.Candidate;
 import vn.edu.iuh.fit.backend.repository.CandidateRepository;
 import vn.edu.iuh.fit.backend.services.CandidateService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class CandidateController {
@@ -13,9 +21,27 @@ public class CandidateController {
     private CandidateRepository candidateRepository;
     @Autowired
     private CandidateService candidateServices;
-    @GetMapping("/candidates")
+    @GetMapping("/categories")
     public String showCandidateList(Model model) {
         model.addAttribute("candidates", candidateRepository.findAll());
         return "candidates";
+    }
+    @GetMapping("/candidatePages")
+    public String showCandidateListPaging(Model model,
+                                          @RequestParam("page") Optional<Integer> page,
+                                          @RequestParam("size") Optional<Integer> size) {
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(10);
+        Page<Candidate> candidatePage= candidateServices.findAll(
+                currentPage - 1,pageSize,"id","asc");
+        model.addAttribute("candidatePage", candidatePage);
+        int totalPages = candidatePage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+        return "candidates-paging";
     }
 }
